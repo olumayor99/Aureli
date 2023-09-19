339cd49cbfe7e608888b47eed09c58eef22bc170
olumayor99 <olumayoor99@gmail.com>
2023-09-20 00:58:14 +0300

    removed hidden .git folder

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MessageComponent from './MessageComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MessageComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/MessageComponent.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+const MessageComponent = () => {
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+      fetch(process.env.REACT_APP_BACKEND_URL)
+         .then((response) => {
+            if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+         .then((data) => {
+            console.log(data);
+            setMessage(data.message);
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+   }, []);
+
+  return (
+    <div>
+      <h1>Message from the Backend:</h1>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default MessageComponent;
